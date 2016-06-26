ea47e0ef8a7f6342c9850e63539d0b2f2039d243
JuKu <kuenzel.justin@t-online.de>
2016-06-26 17:12:28 +0200

    change autoloader.

--- framework/lib/inc/autoload.php
+++ framework/lib/inc/autoload.php
@@ -6,8 +6,11 @@
 
 class PFS_Autoloader {
 
-    //counter of loaded classes, for statistics
-    protected static $loaded_classes = 0;
+    //array with loaded classes, for statistics
+    protected static $loaded_classes = array();
+
+    //array with all not founded classes
+    protected static $error_logs = array();
 
     /**
      * return number of loaded classes
@@ -15,18 +18,38 @@ class PFS_Autoloader {
      * @return number of loaded classes
      */
     public static function countLoadedClasses () {
-        return self::loaded_classes;
+        return count(self::loaded_classes);
     }
 
     /**
      * try to load class with autoloader
      */
-    public function loadClass ($class_name) {
+    public static function loadClass ($class_name) {
         $loaded = false;
+
+        //convert to array for namespaces
+        $array1 = explode("_", $class_name);
+
+        if (sizeof($array1) == 1) {
+            //check, if file exists
+            if (file_exists(LIB_PSF_ROOT . "inc/classes/" . $class_name . ".php")) {
+                //include class
+                require(LIB_PSF_ROOT . "inc/classes/" . $class_name . ".php");
+
+                //add class to array
+                self::$loaded_classes[] = LIB_PSF_ROOT . "inc/classes/" . $class_name . ".php";
+
+                //set loaded flag to true, class was loaded
+                $loaded = true;
+            }
+        }
 
         return $loaded;
     }
 
 }
+
+//register autoloader
+spl_autoload_register("FPS_Autoloader::load_class");
 
 ?>
