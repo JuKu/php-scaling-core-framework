ff1e3dec7de9f9d28281aff04ca109d94b0d6216
JuKu <kuenzel.justin@t-online.de>
2016-07-11 23:59:50 +0200

    set required php version to 7.0.8, because version 7.0.7 had an security issue.

--- framework/lib/inc/lib.php
+++ framework/lib/inc/lib.php
@@ -8,6 +8,6 @@ define('FPS_LIB_VERSION', "0.0.1");
 define('FPS_LIB_WEBSITE', "https://github.com/JuKu/php-scaling-core-framework");
 
 //PHP 7 is required
-define('FPS_MIN_PHP_VERSION', "7.0.7");
+define('FPS_MIN_PHP_VERSION', "7.0.8");
 
 ?>
