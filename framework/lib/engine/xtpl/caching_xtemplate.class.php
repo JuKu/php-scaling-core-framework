61ca26d9f93148a6818eee12c6168917acd4e83a
JuKu <kuenzel.justin@t-online.de>
2016-07-12 23:32:33 +0200

    fixed some problems.

--- framework/lib/engine/xtpl/caching_xtemplate.class.php
+++ framework/lib/engine/xtpl/caching_xtemplate.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'xtemplate.class.php');
+//require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'xtemplate.class.php');
 
 /**
  * CachingXTemplate
