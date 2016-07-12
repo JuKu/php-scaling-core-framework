4e44436e380e536543f659c04a0d527d3f3004d6
JuKu <kuenzel.justin@t-online.de>
2016-07-13 00:04:51 +0200

    added memcache configuration.

---
+++ framework/lib/config/memcache.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Memcache configuration
+ *
+ * PSF Framework doesnt require memcache, but you can use it to speed up your website.
+ * Note: If you want to use memcache, you have also to configure in file cache.php
+ */
+
+$memcache = array(
+    'host' => "127.0.0.1",
+    'port' => "11211",
+    'authentification' => array(
+        'enabled' => false,
+        'user' => "",
+        'password' => ""
+    )
+);
+
+?>
