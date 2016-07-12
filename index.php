f43455f41cf9ca338acfc7835486423574a5e6af
JuKu <kuenzel.justin@t-online.de>
2016-07-12 23:08:30 +0200

    fixed some problems.

--- index.php
+++ index.php
@@ -10,9 +10,17 @@ define('ROOT_PATH', dirname(__FILE__) . "/");
 
 require(ROOT_PATH . "framework/lib/inc/init.php");
 
-//your application code here
+try {
+    //your application code here
 
-//...
+    //...
+} catch (Exception $e) {
+    echo $e->getTraceAsString();
+
+    //clear caches to avoid other exceptions
+    Cache::getCache()->clear();
+    Cache::get2ndLvlCache()->clear();
+}
 
 $end_time = microtime(true);
 $time_diff = $end_time - $start_time;
