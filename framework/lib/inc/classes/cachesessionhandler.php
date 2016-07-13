1fe52989153af6cd19f709970302a5e5693e200d
JuKu <kuenzel.justin@t-online.de>
2016-07-13 18:39:48 +0200

    added CacheSessionHandler.

---
+++ framework/lib/inc/classes/cachesessionhandler.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Justin
+ * Date: 13.07.2016
+ * Time: 18:15
+ */
+class CacheSessionHandler implements SessionHandlerInterface{
+
+    //http://php.net/manual/de/class.sessionhandlerinterface.php
+
+    protected $session_cache = null;
+
+    public function __construct() {
+        //check, if session cache exists
+        if (Cache::containsCache("session_cache")) {
+            //get session cache
+            $this->session_cache = Cache::getCache("session_cache");
+        } else {
+            //use first level cache
+            $this->session_cache = Cache::getCache();
+        }
+    }
+
+    /**
+     * Close the session
+     * @link http://php.net/manual/en/sessionhandlerinterface.close.php
+     * @return bool <p>
+     * The return value (usually TRUE on success, FALSE on failure).
+     * Note this value is returned internally to PHP for processing.
+     * </p>
+     * @since 5.4.0
+     */
+    public function close() {
+        return true;
+    }
+
+    /**
+     * Destroy a session
+     * @link http://php.net/manual/en/sessionhandlerinterface.destroy.php
+     * @param string $session_id The session ID being destroyed.
+     * @return bool <p>
+     * The return value (usually TRUE on success, FALSE on failure).
+     * Note this value is returned internally to PHP for processing.
+     * </p>
+     * @since 5.4.0
+     */
+    public function destroy($session_id) {
+        $this->session_cache->clear("sessions", "session_" . $session_id);
+
+        return true;
+    }
+
+    /**
+     * Cleanup old sessions
+     * @link http://php.net/manual/en/sessionhandlerinterface.gc.php
+     * @param int $maxlifetime <p>
+     * Sessions that have not updated for
+     * the last maxlifetime seconds will be removed.
+     * </p>
+     * @return bool <p>
+     * The return value (usually TRUE on success, FALSE on failure).
+     * Note this value is returned internally to PHP for processing.
+     * </p>
+     * @since 5.4.0
+     */
+    public function gc($maxlifetime) {
+        return true;
+    }
+
+    /**
+     * Initialize session
+     * @link http://php.net/manual/en/sessionhandlerinterface.open.php
+     * @param string $save_path The path where to store/retrieve the session.
+     * @param string $name The session name.
+     * @return bool <p>
+     * The return value (usually TRUE on success, FALSE on failure).
+     * Note this value is returned internally to PHP for processing.
+     * </p>
+     * @since 5.4.0
+     */
+    public function open($save_path, $name) {
+        return true;
+    }
+
+    /**
+     * Read session data
+     * @link http://php.net/manual/en/sessionhandlerinterface.read.php
+     * @param string $session_id The session id to read data for.
+     * @return string <p>
+     * Returns an encoded string of the read data.
+     * If nothing was read, it must return an empty string.
+     * Note this value is returned internally to PHP for processing.
+     * </p>
+     * @since 5.4.0
+     */
+    public function read($session_id) {
+        if ($this->session_cache->contains("sessions", "session_" . $session_id)) {
+            $data = $this->session_cache->get("sessions", "session_" . $session_id);
+
+            if ($data) {
+                return $data;
+            } else {
+                //session not found
+                return "";
+            }
+        } else {
+            //session not found
+            return "";
+        }
+    }
+
+    /**
+     * Write session data
+     * @link http://php.net/manual/en/sessionhandlerinterface.write.php
+     * @param string $session_id The session id.
+     * @param string $session_data <p>
+     * The encoded session data. This data is the
+     * result of the PHP internally encoding
+     * the $_SESSION superglobal to a serialized
+     * string and passing it as this parameter.
+     * Please note sessions use an alternative serialization method.
+     * </p>
+     * @return bool <p>
+     * The return value (usually TRUE on success, FALSE on failure).
+     * Note this value is returned internally to PHP for processing.
+     * </p>
+     * @since 5.4.0
+     */
+    public function write($session_id, $session_data) {
+        $this->session_cache->put("sessions", "session_" . $session_id, $session_data, Host::getSessionTTLInMinutes());
+
+        return true;
+    }
+}
