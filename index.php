5b8034c5cb7467891a089e7ab578061cbb5a99b5
JuKu <kuenzel.justin@t-online.de>
2016-07-13 22:51:17 +0200

    change index.php.

--- index.php
+++ index.php
@@ -13,13 +13,11 @@ require(ROOT_PATH . "framework/lib/inc/init.php");
 try {
     //your application code here
 
-    $headers = apache_request_headers();
+    /*$headers = apache_request_headers();
 
     foreach ($headers as $header => $value) {
         echo "$header: $value <br />\n";
-    }
-
-    print_r(apache_response_headers());
+    }*/
 
     //...
 } catch (Exception $e) {
