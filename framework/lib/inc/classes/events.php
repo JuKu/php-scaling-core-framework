e57d84dd5788e559d17271bafb90ffa3a5eae342
JuKu <kuenzel.justin@t-online.de>
2016-07-14 18:13:45 +0200

    change class Events.

--- framework/lib/inc/classes/events.php
+++ framework/lib/inc/classes/events.php
@@ -8,12 +8,34 @@
  */
 class Events {
 
+    protected static $events = array();
+
+    protected static $isInitialized = false;
+
     public static function init () {
         //
     }
 
     public static function throwEvent ($name, $params = array()) {
-        //
+        if (!is_array($params)) {
+            throw new IllegalArgumentException("second parameter params has to be an array.");
+        }
+
+        //check, if events was initialized first
+        if (!self::$isInitialized) {
+            //initialize events
+            self::init();
+        }
+
+        if (isset(self::$events[$name])) {
+            foreach (self::$events as $event) {
+                self::executeEvent($event, $params);
+            }
+        }
+    }
+
+    protected static function executeEvent ($row, $params) {
+        //TODO: execute event here
     }
 
 }
