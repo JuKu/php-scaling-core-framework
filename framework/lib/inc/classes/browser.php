2475da3eb39fd75c30bf2d5b85b6ab1c6ea8ba0e
JuKu <kuenzel.justin@t-online.de>
2016-08-10 20:31:32 +0200

    change class Browser, added support for plugins.

--- framework/lib/inc/classes/browser.php
+++ framework/lib/inc/classes/browser.php
@@ -25,7 +25,14 @@ class Browser {
     }
 
     public function getUserAgent () {
-        return htmlentities($_SERVER['HTTP_USER_AGENT']);
+        $user_agent = htmlentities($_SERVER['HTTP_USER_AGENT']);
+
+        //throw event, so plugins can modify user agent
+        Events::throwEvent("get_user_agent", array(
+            'user_agent' => &$user_agent,
+        ));
+
+        return $user_agent;
     }
 
 }
