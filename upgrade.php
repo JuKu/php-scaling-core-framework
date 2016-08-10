946350209c166150cef5ff2c119517c762db2332
JuKu <kuenzel.justin@t-online.de>
2016-08-10 19:31:27 +0200

    change upgrade script.

--- upgrade.php
+++ upgrade.php
@@ -10,8 +10,15 @@
 //define root path
 define('ROOT_PATH', dirname(__FILE__) . "/");
 
-//define lib path
+//define lib root path
 define('LIB_PSF_ROOT', dirname(__FILE__) . "/framework/lib/");
+
+//define lib paths
+define('LIB_PSF_CACHE', LIB_PSF_ROOT . "../cache/");
+define('LIB_PSF_CONFIG', LIB_PSF_ROOT . "config/");
+define('LIB_PSF_STORE', LIB_PSF_ROOT . "store/");
+define('LIB_PSF_PACKAGES', LIB_PSF_ROOT . "packages/");
+define('LIB_PSF_PLUGINS', LIB_PSF_ROOT . "plugins/");
 
 //allow error reporting
 error_reporting(E_ALL);
